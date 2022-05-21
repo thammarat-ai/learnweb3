13960704c6787f4eab47f977c0902834a4e187d8
Thammarat Thamma <84767311+thammarat-ai@users.noreply.github.com>
2022-05-21 16:25:59 +0700

    Add files via upload

---
+++ mood.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+contract MoodDiary {
+
+    string mood;
+
+    // This is going to set mood
+    function setMood(string memory _mood) public{
+        mood = _mood;
+
+    }
+
+    //This is going to set mood
+    function getMood() public view returns(string memory) {
+        return mood;
+    }
+}
